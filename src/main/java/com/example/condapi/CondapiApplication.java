39eb1b76e5865c342f4518291f3ab0fbb6032f30
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2025-04-15 07:37:38 -0300

    initial commit

---
+++ src/main/java/com/example/condapi/CondapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.condapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondapiApplication.class, args);
+	}
+
+}
