c0fb668a2e6ac7e7124f9b123a1be6e2a72a507e
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2025-04-22 08:22:16 -0300

    criando e modificando classes

--- src/main/java/com/example/condapi/model/entity/UsuarioAdministrador.java
+++ src/main/java/com/example/condapi/model/entity/UsuarioAdministrador.java
@@ -24,12 +24,10 @@ public class UsuarioAdministrador {
     private String cnpj;
     private String nomeResponsavelComercial;
     private String telefoneComercial;
-    private String email;
     private String logradouro;
     private int numero;
     private String complemento;
     private String bairro;
     private String uf;
     private String cep;
-    private String senha;
 }
