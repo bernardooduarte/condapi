487938e9f554f5cc501296b2f43140628a4a22de
Bernardo Duarte <bernardoduarte.mg@gmail.com>
2025-06-25 15:25:33 -0300

    correção BlocoDTO

--- src/main/java/com/example/condapi/api/dto/BlocoDTO.java
+++ src/main/java/com/example/condapi/api/dto/BlocoDTO.java
@@ -9,7 +9,6 @@ import org.modelmapper.ModelMapper;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class BlocoDTO {
 
     private Long id;
@@ -21,7 +20,21 @@ public class BlocoDTO {
     public static BlocoDTO create(Bloco bloco){
         ModelMapper modelMapper = new ModelMapper();
         BlocoDTO dto = modelMapper.map(bloco, BlocoDTO.class);
-        dto.nomeCondominio = bloco.getCondominio().getNome();
+
+        // Adicionar verificação de nulidade para Condominio
+        if (bloco.getCondominio() != null) {
+            dto.nomeCondominio = bloco.getCondominio().getNome();
+        } else {
+            dto.nomeCondominio = null; // Ou "" ou algum valor padrão se Condominio for null
+        }
+
+        // Se você também precisa do ID do Condomínio no DTO
+        if (bloco.getCondominio() != null) {
+            dto.idCondominio = bloco.getCondominio().getId();
+        } else {
+            dto.idCondominio = null;
+        }
+
         return dto;
     }
 }
